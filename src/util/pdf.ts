5417134152ec323928ec94e91235ada20f2227d9
Charlie <charlie.tiehm@gmail.com>
2018-06-26 20:20:29 +0200

    Introduced prettier

--- src/util/pdf.ts
+++ src/util/pdf.ts
@@ -1,6 +1,6 @@
 import * as puppeteer from 'puppeteer';
 
-export async function pdf () {
+export async function pdf() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/');
@@ -9,9 +9,18 @@ export async function pdf () {
     const height = await page.evaluate(() => {
         const body = document.body,
             html = document.documentElement;
-        return Math.max (body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+        return Math.max(
+            body.scrollHeight,
+            body.offsetHeight,
+            html.clientHeight,
+            html.scrollHeight,
+            html.offsetHeight
+        );
     });
-    await page.pdf({path: 'page.pdf', height: (parseInt(height) + 100).toString()  + 'px'});
+    await page.pdf({
+        path: 'page.pdf',
+        height: (parseInt(height) + 100).toString() + 'px'
+    });
     await browser.close();
-    return Promise.resolve(true);
+    return true;
 }
--- src/util/renderer.ts
+++ src/util/renderer.ts
@@ -1,23 +1,19 @@
 import * as express from 'express';
-import * as exphbs  from 'express-handlebars';
-import { pdf }      from './pdf';
+import * as exphbs from 'express-handlebars';
+import { pdf } from './pdf';
 
-export function setup (data: Setup) {
-
+export function setup(data: Setup) {
     const app: express.Application = express();
     app.engine('handlebars', exphbs());
     app.set('view engine', 'handlebars');
 
     app.get('/', (req, res) => {
-
         res.render('index', data);
-
     });
 
     app.listen(3000, async () => {
         await pdf();
-        console.log('PDF created.')
+        console.log('PDF created.');
         process.exit(0);
-    })
-
+    });
 }
